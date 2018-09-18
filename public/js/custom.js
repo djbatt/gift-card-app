edc6494e9444f2b8d0a8a80a4f39420e7efe3c65
David Battiston <djbatt@live.com>
2018-09-17 20:22:35 -0400

    Deleted old front end js, convert to a react UI after

--- public/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({
-      draggable: true,
-    });
-    
-    $('.modal').modal();
-});
--- src/components/layout/NavBar/NavBar.js
+++ src/components/layout/NavBar/NavBar.js
@@ -70,32 +70,3 @@ export default withAuth(class NavBar extends Component{
         )
     }
 })
-
-// export default class NavBar extends Component {
-//   render() {
-//     return (
-//     <header>
-//         <div className="navbar-fixed">
-//             <nav>
-//                 <div className="nav-wrapper">
-//                     <Link to="/" className="brand-logo">eCards</Link>
-//                     <Link to="/" className="sidenav-trigger" data-target="leftNav">
-//                         <i className="material-icons">menu</i>
-//                     </Link>
-//                     <ul className="right hide-on-med-and-down">
-//                         <li><Link to="/">Home</Link></li>
-//                         <li><Link className="modal-trigger" to="/business">My Business</Link></li>
-//                         <li><Link to="/">Contact Us</Link></li>
-//                     </ul>
-//                 </div>
-//             </nav>
-//         </div>
-//         <ul className="sidenav" id="leftNav">
-//             <li><Link to="/">Home</Link></li>
-//             <li><Link className="modal-trigger" to="/business">My Business</Link></li>
-//             <li><Link to="/">Contact Us</Link></li>
-//         </ul>
-//     </header>
-//     )
-//   }
-// }
