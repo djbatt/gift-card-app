f16aec688ced4630d5304ffba7e5061a9f2c7b0a
David Battiston <djbatt@live.com>
2018-10-22 13:08:49 -0400

    removed existing client architecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import TotalContainer from "./components/container"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+//Page Routes
+import NotFound from './components/layout/404/404';
+import Home from './components/layout/home/home';
+import Dashboard from './components/layout/dashboard/dashboard';
+
+export default class App extends Component {
   render() {
     return (
-      <div>
-        <TotalContainer/>
-      </div>
-    );
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home} />
+          <Route path='/dashboard' component={Dashboard} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    )
   }
 }
-
-export default App;
